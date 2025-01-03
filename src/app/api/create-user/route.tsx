61cccb905a2bdc0259fa54d46deec5ef6df8da1c
Supritphuyal <supritphuyal123@gmail.com>
2025-01-03 21:49:42 +0545

    Added auth model for auth0 and refactored usercreation

--- src/app/api/create-user/route.tsx
+++ src/app/api/create-user/route.tsx
@@ -7,21 +7,30 @@ const prisma = new PrismaClient();
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-
     const { email, name, password } = body;
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await prisma.user.create({
+    const auth = await prisma.auth.create({
       data: {
         email,
         name,
-        password: hashedPassword, 
+        password: hashedPassword,
       },
     });
-    console.log("User created", user);
-    return NextResponse.json(user, { status: 201 });
+
+    const user = await prisma.user.create({
+      data: {
+        auth: {
+          connect: { id: auth.id },
+        },
+      },
+    });
+
+    console.log("User and Auth created", { auth, user });
+    return NextResponse.json({ auth, user }, { status: 201 });
   } catch (error) {
-    console.error("Error creating user", error);
-    return NextResponse.json({ error: "Error creating user" }, { status: 500 });
+    console.error("Error creating user and auth", error);
+    return NextResponse.json({ error: "Error creating user and auth" }, { status: 500 });
   }
 }
