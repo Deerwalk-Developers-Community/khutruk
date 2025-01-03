12be843bde6b4e92ab949c867315f592cb900af6
Supritphuyal <supritphuyal123@gmail.com>
2025-01-03 18:34:32 +0545

    Completed User Creation

--- src/app/api/create-user/route.tsx
+++ src/app/api/create-user/route.tsx
@@ -1,25 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
-export async function POST(req: any){
-    try{
-        const body = await req.json();
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
 
-        const { email, name, password } = body;
+    const { email, name, password } = body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const user = await prisma.user.create({
-            data: {
-              email,
-              name,
-            },
-          })
-          return NextResponse.json(user, {status: 201})
-    }catch (error){
-        console.error('Error creating user', error)
-        return NextResponse.json({error: 'Error creating user'}, {status: 500})
-    }finally{
-        await prisma.$disconnect();
-    }
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword, 
+      },
+    });
+    console.log("User created", user);
+    return NextResponse.json(user, { status: 201 });
+  } catch (error) {
+    console.error("Error creating user", error);
+    return NextResponse.json({ error: "Error creating user" }, { status: 500 });
+  }
 }
