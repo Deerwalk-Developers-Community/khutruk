e64c7dada8807d34284c413918fc44d88cbad148
chrisdvkta <chrisdevkota77@gmail.com>
2025-01-03 18:25:33 +0545

    add:IPFS test

---
+++ src/app/create/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { pinata } from "@/utils/ipfsConfig";
+import React, { useState } from "react";
+
+const Page = () => {
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleIPFSSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!file) {
+      console.error("No file selected");
+      return;
+    }
+
+    try {
+      console.log("entry");
+
+      const fileData = new FormData();
+      fileData.append("file", file);
+
+      const upload = await pinata.upload.file(file);
+      console.log(upload);
+    } catch (err) {
+      console.error("Error while uploading file", err);
+    }
+  };
+
+  return (
+    <div>
+      <Input
+        type="file"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setFile(e.target.files?.[0] ?? null)
+        }
+      />
+      <Button type="submit" onClick={handleIPFSSubmit}>
+        Upload to IPFS
+      </Button>
+    </div>
+  );
+};
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  router.push("/login");
+  // const router = useRouter();
+  // router.push("/login");
+  return <div>test</div>;
 }
