e93eabd3b181391db40fdf77e4ebc5397a934219
Samrat Nakarmi <samratnakarmi1@gmail.com>
2025-01-03 18:40:42 +0545

    add: auth repositories and pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  router.push("/login");
+  // const router = useRouter();
+  // router.push("/login");
 }
