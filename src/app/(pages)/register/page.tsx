bdb62042b0777f365161d0a4484e727a6b59856c
ghostuf <srijan102dahal@gmail.com>
2025-01-04 21:51:15 +0545

    add: useAuth

--- src/app/(pages)/register/page.tsx
+++ src/app/(pages)/register/page.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import RegisterForm from "./components/RegisterForm";
+import Link from "next/link";
 
 const RegisterPage = () => {
   return (
     <section>
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <RegisterForm />
-      </div>
-      <div className="text-sm">
+        <div className="text-sm pl-1 mt-2">
         <p>
-          New to Khutruke? <span className="text-pink"></span>
+          Already have an account? <span className="text-pink"><Link href={'/login'}>Log In</Link></span>
         </p>
       </div>
+      </div>
+      
     </section>
   );
 };
