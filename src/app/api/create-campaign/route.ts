3f608069c0a6eea53a0ebe2f46648e212afdcc32
Samrat Nakarmi <samratnakarmi1@gmail.com>
2025-01-04 13:40:01 +0545

    add: campaign form

--- src/app/api/create-campaign/route.ts
+++ src/app/api/create-campaign/route.ts
@@ -1,34 +1,36 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export async function POST(req: any){
-    try{
-        const data = await req.json();
+export async function POST(req: any) {
+  try {
+    const data = await req.json();
 
-        const campaign = await prisma.campaign.create({
-            data: {
-              creatorId: data.creatorId,
-              title: data.title,
-              description: data.description,
-              category: data.category,
-              campaignAddress: data.campaignAddress,
-              targetAmount: parseFloat(data.targetAmount),
-              startDate: new Date(data.startDate), 
-              endDate: new Date(data.endDate), 
-              status: data.status,
-              mediaUrls: data.mediaUrls,
-            },
-        });
+    const campaign = await prisma.campaign.create({
+      data: {
+        creatorId: data.creatorId,
+        title: data.title,
+        description: data.description,
+        category: data.category,
+        campaignAddress: data.campaignAddress,
+        targetAmount: parseFloat(data.targetAmount),
+        status: "PENDING",
+        mediaUrls: data.mediaUrls,
+      },
+    });
 
-        return new Response(JSON.stringify({ message: "Campaign created", campaign }), {
-            status: 201,
-        });
-          
-    }catch (error) {
-        console.error(error);
-        return new Response(JSON.stringify({message: "unable to create a campaign"}), {
-            status : 500,
-        });
-    }
+    return new Response(
+      JSON.stringify({ message: "Campaign created", campaign }),
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ message: "unable to create a campaign" }),
+      {
+        status: 500,
+      }
+    );
+  }
 }
-
